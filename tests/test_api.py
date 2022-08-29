4954e42503ed8c26b613236cf18b481f4204f9c6
vikctar <vikc.steve@gmail.com>
2022-08-29 16:21:54 +0300

    test record trip endpoint

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,9 +5,24 @@ from tests.configure import BaseTest
 
 class ApiTestCase(BaseTest):
     """This class represents the api test case"""
+
+    new_trip = {
+        "vehicle_id": 1,
+        "driver_id": 1,
+        "customer_id": 1,
+        "address_type": "PICKUP_POINT",
+        "done_by_user_id": 1,
+        "address": "Mombasa",
+        "cargo_tonnage": 1000.00
+    }
+
     def test_demo_route(self):
         response = self.client().get('/api/v1/demo')
         self.assertEqual(response.status_code, 200)
+
+    def test_record_trip(self):
+        response = self.client().post('/api/v1/record-trip', json=self.new_trip)
+        self.assertEqual(response.status_code, 201)
 
 
 if __name__ == '__main__':
